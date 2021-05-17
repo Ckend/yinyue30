7a4137d6df9c5138c858e01fd6bed53d46653b53
ckend <83493903@qq.com>
2021-05-17 23:57:41 +0800

    feature: support cloud develop

--- yinyue30/settings.py
+++ yinyue30/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '%i&wr-m(n$j)#vb1_wy8gbyi(jqrj_rud_i48bhi_)-=hnsvks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
